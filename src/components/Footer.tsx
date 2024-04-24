66e8d375b122b4067b5596742736626c5df84f0a
SuperJu5tin <superjuslib@gmail.com>
2024-04-24 10:57:51 -0600

    Footer redesign and Added paragraph to Home

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+// import Button from '@mui/material/Button'
+// import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 const Footer = () => {
   return (
@@ -11,19 +11,46 @@ const Footer = () => {
       background:"#777",
       border:"solid 5px",
       display:"grid",
-      gridTemplateColumns:"1fr 1fr 1fr",
-      justifyItems:"center",
-      alignItems:"center",
-      textAlign:"center",
+      gridTemplateColumns:"1fr 1fr",
+      gap:"1em",
+      // justifyItems:"center",
+      // alignItems:"center",
+      // textAlign:"center",
     }}>
-      <h4 style={{color:"black"}}>All Rights Reserved @ 2024 justinlibhart.xyz</h4>
-      <Box></Box>
+      <Box sx={{
+        justifySelf:"left",
+        alignSelf:"center"
+      }}>
+        <h4 style={{color:"black"}}>All Rights Reserved @ 2024 justinlibhart.xyz</h4>
+      </Box>
+      
+      {/* <Box></Box> */}
       <Box>
         <h4 style={{color:"black"}}>Contact Me</h4>
-        <Box sx={{display:"flex", gap:"10px", flexDirection:"column"}}>
-          <Box sx={{display:"flex", alignItems:"baseline", gap:"10px", flexWrap:"wrap", flexDirection:"row", justifyContent:"center"}}>
+        <Box sx={{
+          display:"flex", 
+          gap:"10px",
+          flexWrap:"wrap",
+          // flexDirection:"column",
+          alignItems:"baseline", 
+          alignContent:"bottom", 
+          justifyItems:"right",
+          justifyContent:"center",
+        }}>
+          <Box sx={{
+            // display:"flex", 
+            // gap:"10px", 
+            // flexWrap:"wrap",
+            // flexDirection:"row",
+            // alignItems:"baseline", 
+            // justifyItems:"right",
+            // justifyContent:"right",
+            // display:"grid",
+            // gridAutoFlow:"column",
+            // gap:"10px"
+          }}>
             <a href="mailto:superjuslib@gmail.com" >superjuslib@gmail.com</a>
-            <Button size="small" variant="contained" onClick={() => {navigator.clipboard.writeText("superjuslib@gmail.com")}} startIcon={<ContentCopyIcon />}>Copy</Button>
+            {/* <Button variant="contained" onClick={() => {navigator.clipboard.writeText("superjuslib@gmail.com")}}><ContentCopyIcon sx={{fontSize:"1em"}} /></Button> */}
           </Box>
           <a target='_blank' href="https://www.linkedin.com/in/justin-libhart-798498304/" >LinkedIn</a>
           <a target='_blank' href='https://github.com/SuperJu5tin'>Github</a>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,30 +6,39 @@ const Home = () => {
 
   return (
     <Box sx={{
-      height:"60vh",
-      padding:"40px",
+      minHeight:"60vh",
+      // padding:"40px",
+      paddingTop:"5em",
+      paddingBottom:"5em",
+      paddingLeft:"3em",
+      paddingRight:"3em",
       display:"flex",
       justifyContent:"center",
       alignContent:"center",
+      justifyItems:"center",
+      alignItems:"center",
       flexWrap:"wrap",
+      gap:"2em",
     }}>
       <img src={pictureOfMe} style={{
         boxShadow: "10px 5px 5px black",
         height:"min(400px, 57vmin)",
         width:"min(550px, 70vmin)",
-        borderRadius:"20px"
+        borderRadius:"20px",
       }}/>
       <Box sx={{
-        marginLeft:"30px",
-        marginTop:"2em",
+        // marginLeft:"30px",
+        // marginTop:"2em",
         display:"flex",
         flexDirection:"column",
         alignItems:"center",
-        justifyContent:"center"
+        justifyContent:"center",
+        flex:"1"
       }}>
-        <h1 style={{fontFamily:"Satisfy"}}>Hello, I am Justin Libhart</h1>
+        <h1 style={{fontFamily:"Satisfy", borderBottom:"solid .1em"}}>Hello, I am Justin Libhart</h1>
         <h4>I am currently a high school student.</h4>
-        <p style={{textWrap:"pretty"}}>I am a student at both Warren Tech and Standley Lake High School</p>
+        <p style={{textWrap:"wrap"}}>Currently a student at both Warren Tech and Standley Lake High School. I am a hard-working learner, always open to new ways to do things more efficiently. I love working on projects and finding different ways to make my brain hurt. With an extensive knowledge of general Information Technology, from Coding to DevOps to physical Hardware.</p>
+        {/* <p style={{textWrap:"wrap"}}></p> */}
       </Box>
     </Box>
   )
